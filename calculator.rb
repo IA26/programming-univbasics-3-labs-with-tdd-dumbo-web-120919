3aa162dbb77af86a417a2661190e6dd23140be3e
Isaac Avilez <isaac1180@aol.com>
2019-11-21 23:07:15 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,5 +3,5 @@ first_number = 5
 second_number = 4
 sum = first_number + second_number 
 difference = first_number - second_number
-multiply = first_number * second_number
+product = first_number * second_number
 division = first_number / second_number
